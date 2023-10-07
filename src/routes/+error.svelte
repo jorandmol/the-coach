5736c77588b8ebc513d46be92aabeb656efa6526
jorandmol <jorandmol.dev@gmail.com>
2023-10-07 20:11:10 +0200

    🚀 Add custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="container h-full mx-auto flex flex-col justify-center items-center">
+	<h1 class="text-3xl text-primary-900-50-token">{$page.status}</h1>
+	<p class="text-xl">{$page.error?.message ?? 'Oops!'}</p>
+</div>
